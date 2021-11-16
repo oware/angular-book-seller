6db5bb610ba8ecab2165fd82ab8a7cdb40472074
Allan Oware <allanoware@gmail.com>
2021-11-16 13:17:33 +0300

    more components

---
+++ src/app/admin/admin/admin.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
+import { BookService } from "../../services/book.service";
+import { BookComponent } from '../book/book.component';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  bookList: Array<Book> = [];
+
+  @ViewChild(BookComponent) child: BookComponent | undefined;
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.bookService.getAllBooks().subscribe(data => {
+      this.bookList = data;
+    });
+  }
+
+  createBookRequest(){
+    this.child?.showBookModal();
+  }
+
+}
